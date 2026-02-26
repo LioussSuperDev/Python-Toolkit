a21959b61ea3c5b778ebabad408b65938f36d625
jdiscala <jules.di-scala@univ-tlse3.fr>
2026-02-26 16:11:06 +0100

    little modif

--- src/liouss_python_toolkit/utility.py
+++ src/liouss_python_toolkit/utility.py
@@ -7,7 +7,11 @@ import shlex
 from typing import Optional
 
 def save_data(data, full_path:Optional[str] = None, folder:Optional[str] = None, file:Optional[str] = None):
-    if full_path == None:
+    if full_path is None:
+        if folder is None:
+            return
+        if file is None:
+            return
         if not os.path.isdir(folder):
             os.makedirs(folder)
         full_path = os.path.join(folder, file)
@@ -16,25 +20,32 @@ def save_data(data, full_path:Optional[str] = None, folder:Optional[str] = None,
         json.dump(data, f)
         
 def load_data(full_path = None, folder = None, file = None):
-    if full_path == None:
+    if full_path is None:
+        if folder is None:
+            return
+        if file is None:
+            return
         full_path = os.path.join(folder, file)
         
     with open(full_path, "r") as f:
         return json.load(f)
     
-def edit_in_editor(initial_text: str = "", ignore_lines=0, path=None) -> str:
+def edit_in_editor(initial_text: Optional[str] = None, ignore_lines=0, path=None) -> str:
     editor = os.environ.get("EDITOR") or os.environ.get("VISUAL") or "nano"
     path_none = path is None
     if path is None:
         with tempfile.NamedTemporaryFile(mode="w+", suffix=".temp.txt", delete=False, encoding="utf-8") as f:
             path = Path(f.name)
-            f.write(initial_text)
+            f.write(initial_text if initial_text is not None else "")
             f.flush()
-    else:
-        with open(path, 'w+') as f:
+    elif initial_text is not None:
+        with open(path, 'w') as f:
             path = Path(f.name)
             f.write(initial_text)
             f.flush()
+    else:
+        path = Path(path)
+        
     try:
         cmd = shlex.split(editor) + [str(path)]
         subprocess.run(cmd, check=False)
